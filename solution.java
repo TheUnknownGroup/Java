a100f6931fecd25ce58b8d7a198560fd3467234a
xGpD <94760159+xGpD@users.noreply.github.com>
2023-03-03 20:55:56 -0500

    Update solution.java

--- solution.java
+++ solution.java
@@ -1,89 +1,66 @@
 import java.util.Scanner;
-public class solutio
+public class Solution
 {
-  public static void main(String[] args){
+    public static void main(String[] args) {
 
-    String line ="------";
-    String correct ="Congrats, You got this question correct! You gained a point!";
-    String wrongr ="Whoops, you got this question wrong. You lost a point.";
-    String p3 ="You got all of the questions correct!";
-    String p2 ="You only got 1 question wrong!";
-    String p1 ="You got 2 questions wrong. It's alright, just try harder next time.";
-    String p0 ="You got all of the questions wrong...How?";
-    String po1 ="HOW DID YOU GET NEGATIVE POINTS???!!!!";
-    String pointsNumber ="Here are your points: ";
-    
-    String welcome="Welcome! This is a trivia game. Would you like to continue?";
-    String question1="What is the element name for Fe";
-    String question2="What are the positive atoms?";
-    String question3="What are the negative atoms?";
-    
-    String answer1="Iron";
-    String answer2="Protons";
-    String answer3="Electrons";
-    String welcomeYes="Yes";
+        String question1 = "This question has how many words?";
+        String question2 = "What is the chemical symbol for Iron";
+        String question3 = "How many states are there in the USA?";
 
-    int points=0;
-    
-    Scanner input = new Scanner(System.in);
-      System.out.println(welcome);
-      String userInputWelcome = input.next();
+        int answer1 = 6;
+        String answer2 = "Fe"; 
+        int answer3 = 50;
 
-      if (userInputWelcome.equals(welcomeYes)){
-        System.out.println("You have picked 'Yes', you will now proceed to the questions area.");
-        System.out.println(line);
+        int points = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(question1);
-      } else {
-        System.out.println("You have picked 'No', you will not continue with the following questions.");
-        System.exit(0);
-      }
-      String userInput1 = input.next();
-      
-      if (userInput1.equals(answer1)){
-        points++;
-        System.out.println(correct);
-      } else {
-        points--;
-        System.out.println(wrongr);
-      }
+        int userInput1 = scanner.nextInt();
 
-      System.out.println(line);
-      System.out.println(question2);
-      String userInput2 = input.next();
-      
-      if(userInput2.equals(answer2)) {
-        points++;
-        System.out.println(correct);
-      } else {
-        points--;
-        System.out.println(wrongr);
-      }
-      
-      System.out.println(line);
-      System.out.println(question3);
-      String userInput3 = input.next();
+        if (userInput1 == answer1) {
+            points++;
+            System.out.println("Congrats, You got this question correct! You have " + points + " point!");
+        } else {
+            points--;
+            System.out.println("Oh Sorry, You got this wrong. You have " + points + " points.");
+        }
 
-      if(userInput3.equals(answer3)) {
-        points++;
-        System.out.println(correct);
-      } else {
-        points--;
-        System.out.println(wrongr);
-      }
+        System.out.println("------");
+        System.out.println(question2);
+        String userInput2 = scanner.next();
 
-      System.out.println(line);
-      System.out.println(pointsNumber +points);
-      
-      if(points == 3) {
-        System.out.println(p3);
-      } else if(points == 2) {
-        System.out.println(p2);
-      } else if(points == 1) {
-        System.out.println(p1);
-      } else if(points == 0) {
-        System.out.println(p0);
-      } else if(points < 0) {
-        System.out.println(po1);
-      }
-  }
+        if(userInput2.equals(answer2)) {
+            points++;
+            System.out.println("Congrats, You got this question correct! You have " + points + " points!");
+        } else {
+            points--;
+            System.out.println("Oh Sorry, You got this wrong. You have " + points + " points.");
+        }
+
+        System.out.println("------");
+        System.out.println(question3);
+        int userInput3 = scanner.nextInt();
+
+        if(userInput3 == answer3) {
+            points++;
+            System.out.println("Congrats, You go this question correct! You have " + points + " points!");
+        } else {
+            points--;
+            System.out.println("Oh Sorry, You got this wrong. You have " + points + " points.");
+        }
+        System.out.println("------");
+        System.out.println("Here are your points: "+points+".");
+        if (points == 3) {
+            System.out.println("You did great!");
+        } else if(points == 2) {
+            System.out.println("You did ok, you only got 1 wrong!");
+        } else if(points == 1) {
+            System.out.println("You still did ok, you only got 2 wrong.");
+        } else if(points == 0) {
+            System.out.println("You got none of them right, please try again later.");
+        } else if(points < 0){
+            System.out.println("HOW DID YOU GET NEGATIVE POINTS??????");
+        }
+    }
 }
