f5575d79378e35b2f88415ab381debf5bfe7a7d8
devonk15 <94760159+devonk15@users.noreply.github.com>
2023-03-23 14:40:09 -0400

    Update RR&TOVStudy.py

--- RR&TOVStudy.py
+++ RR&TOVStudy.py
@@ -1,5 +1,22 @@
 import time
 
-questions = ['','','','','','','','','','']
-
-print(questions.count())
+questions = ['> 1) What is the War Guilt clause?',
+             '> 2) What was Nazism?',
+             '> 3) What is Communism?',
+             '> 4) Who were the Bolsheviks?',
+             '> 5) What was the League of Nations?',
+             '> 6) What is Isolationism?',
+             '> 7) What is Disarmament?',
+             '> 8) What was 14 Points?',
+             '> 9) What is Reparations?',
+             '> 10) What is the Treaty of Brest-Litovsk?']
+answers = ['\'The Allied and Associated Governments affirm, and Germany accepts the responsibility of Germany and her allies for causing all the loss and damage to which the Allied and Associated Governments and their nationals have been subjected as a consequence of the war imposed on them by the aggression of Germany and her allies -> Treaty of Versailles, Article 231\'',
+           'the political principles of the National Socialist German Workers\' Party',
+           'economic and political system in which government owns the means of production and controls economic planning',
+           'Marxists whose goal was to seize state power and establish a dictatorship of the proletariat; Soviet Communists',
+           'an internation body of nations formed after WWI to prevent future wars',
+           'staying out of the affairs and wars of other nations; the position intially held by the US at the beginning of WWII',
+           'the reduction or withdrawal of military forces and weapons',
+           'The Fourteen Points was a statement of principles for peace that was to be used for peace negotiations in order to end World War I.',
+           'Germany had to pay over $33 Billion in Reparations or fines.',
+           'The Treaty of Brest-Litovsk (also known as the Treaty of Brest in Russia) was a separate peace treaty signed on 3 March 1918 between Russia and the Central Powers (Germany, Austria-Hungary, Bulgaria, and the Ottoman Empire), that ended Russia\'s participation in World War I.']
