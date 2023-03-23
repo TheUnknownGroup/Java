bc0f575bb7d2139a1948bdcf9ea57721d0864a49
devonk15 <94760159+devonk15@users.noreply.github.com>
2023-03-23 15:01:02 -0400

    Update RR&TOVStudy.py

--- RR&TOVStudy.py
+++ RR&TOVStudy.py
@@ -10,7 +10,7 @@ questions = ['> 1) What is the War Guilt clause?',
              '> 8) What was 14 Points?',
              '> 9) What is Reparations?',
              '> 10) What is the Treaty of Brest-Litovsk?']
-answers = ['\'The Allied and Associated Governments affirm, and Germany accepts the responsibility of Germany and her allies for causing all the loss and damage to which the Allied and Associated Governments and their nationals have been subjected as a consequence of the war imposed on them by the aggression of Germany and her allies -> Treaty of Versailles, Article 231\'',
+answers = ['\'The Allied and Associated Governments affirm, and Germany accepts the responsibility of Germany and her allies for causing all the loss and damage\'',
            'the political principles of the National Socialist German Workers\' Party',
            'economic and political system in which government owns the means of production and controls economic planning',
            'Marxists whose goal was to seize state power and establish a dictatorship of the proletariat; Soviet Communists',
@@ -19,4 +19,35 @@ answers = ['\'The Allied and Associated Governments affirm, and Germany accepts 
            'the reduction or withdrawal of military forces and weapons',
            'The Fourteen Points was a statement of principles for peace that was to be used for peace negotiations in order to end World War I.',
            'Germany had to pay over $33 Billion in Reparations or fines.',
-           'The Treaty of Brest-Litovsk (also known as the Treaty of Brest in Russia) was a separate peace treaty signed on 3 March 1918 between Russia and the Central Powers (Germany, Austria-Hungary, Bulgaria, and the Ottoman Empire), that ended Russia\'s participation in World War I.']
+           'The Treaty of Brest-Litovsk was a separate peace treaty signed on 3 March 1918 between Russia and the Central Powers, that ended Russia\'s participation in World War I.']
+
+line = '------'
+welcome = 'Welcome! Would you like to continue?'
+tries = 'Would you like to study more?'
+
+def ask_questions():
+    points = 0
+    welcomeYes = input(welcome+'\n')
+    if welcomeYesNo in ['Yes','yes','y','sure']:
+        print('>> You will now go to the questions area since you have picked either \'Yes\' or \'yes\'.')
+        time.sleep(1)
+        print(line)
+    else:
+        print('>> You didn\'t pick either \'Yes\' or \'yes\'. Thank you for your time.')
+        time.sleep(1)
+        exit()
+    ans1 = input(str(questions[0]).lstrip('[\'').rstrip('\']')
+                +str(answers[6]).lstrip('[\'').rstrip('\']')+'\na) '
+                +str(answers[5]).lstrip('[\'').rstrip('\']')+'\nb) '
+                +str(answers[10]).lstrip('[\'').rstrip('\']')+'\nc) '
+                +str(answers[0]).lstrip('[\'').rstrip('\']')+'\nd) ')
+    if ans1 == 'd':
+        points += 1
+        print(f'\nCorrect!{time.sleep(1)}{line}')
+    else:
+        print(f'\nWhoops, got that wrong. The Answer was {answers[0]}{time.sleep(1)}{line}')
+    ans2 = input(str(questions[1]).lstrip('[\'').rstrip('\']')
+                +str(answers[9]).lstrip('[\'').rstrip('\']')+'\na) '
+                +str(answers[1]).lstrip('[\'').rstrip('\']')+'\nb) '
+                +str(answers[3]).lstrip('[\'').rstrip('\']')+'\nc) '
+                +str(answers[5]).lstrip('[\'').rstrip('\']')+'\nd) ')
