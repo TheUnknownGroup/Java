b0d5c17afa880cc8d6f9da4a9ebc95d478e2d1e5
devonk15 <94760159+devonk15@users.noreply.github.com>
2023-03-25 15:31:46 -0400

    Add files via upload

---
+++ bit.py
@@ -0,0 +1,8 @@
+import time
+line = '------'
+
+print(f'Hello. Are you lonely?\n{line}')
+time.sleep(1)
+print(f'Are you looking for bitches (either male or female)?\n{line}')
+time.sleep(1)
+print('Then you\'ve come to the wrong place. Yo bitchass gonna be lonely foreva')
