df87160c4e7d44baea17d5da6a4abb1031416528
devonk15 <94760159+devonk15@users.noreply.github.com>
2023-04-04 12:33:00 -0400

    Update WWIp-2.py

--- WW-/WWIp-2.py
+++ WW-/WWIp-2.py
@@ -1,5 +1,7 @@
 import time
 
+line = '------'
+
 qodQuestions = ['> 1) During World War I, communists seized power in Russia and created a totalitarian state. How were these developments similar to post-World War I developments in other European nations?']
 qodAnswers = ['a) Nearly all European nations adopted communist forms of government after World War I.',
               '\n',
@@ -10,10 +12,16 @@ qodAnswers = ['a) Nearly all European nations adopted communist forms of governm
               'd) Germany and Italy also embraced totalitarian dictatorships in response to economic, political and social problems arising out of World War I.']
 
 def ask_questions():
-    print(qodQuestions[0],'\n')
+    points = 0
+    print(qodQuestions[0])
     for s in qodAnswers:
         print(s, end='')
-
+    an1 = input('\n')
+    if an1 in ['d','D']:
+        print('Correct!\n'); time.sleep(1); print(line); points += 1
+    else:
+        print(f'Whoops, got that wrong. The answer was {qodAnswers[6]}\n'); time.sleep(1); print(line)
+    print(f'You have this many points: {points} out of 1')
 welcome = input('Welcome! Would you like to continue?\n')
 if welcome in ['yes','Yes','y','sure']:
     print('Alrighty then, lets continue to questions area!'); time.sleep(1); ask_questions()
