4a92b60846b089635882411f2fe0a919f099f9ca
devonk15 <94760159+devonk15@users.noreply.github.com>
2023-04-11 13:59:33 -0400

    Update WWIp-2.py

--- WW-/WWIp-2.py
+++ WW-/WWIp-2.py
@@ -5,18 +5,9 @@ line = '------'
 qodQuestions = ['> 1) During World War I, communists seized power in Russia and created a totalitarian state. How were these developments similar to post-World War I developments in other European nations?',
                 '> 2) One impact of the worldwide economic depression that occurred between World War I and World War II was the:',
                 '> 3) How did the death and destruction during World War I shape the postwar policies of France, England and the United States as they were confronted with German aggression in the 1930s?']
-qodAnswers = ['a) Nearly all European nations adopted communist forms of government after World War I.','\n',
-              'b) Communists also seized power in Great Britain and France after World War I.','\n',
-              'c) The formation of the League of Nations can be seen as a similar consolidation of power under one totalitarian governing body.','\n',
-              'd) Germany and Italy also embraced totalitarian dictatorships in response to economic, political and social problems arising out of World War I.']
-qodAns1 = ['a) decline of totalitarian leaders whose militaristic policies led to more costly wars.','\n',
-           'b) rise of totalitarian leaders worldwide who promised their citizens a return to stability through strong leadership and militaristic expansion.','\n',
-           'c) rise of totalitarian leaders worldwide whose message of isolationism and appeasement indicated a lack of concern for foreign affairs.','\n',
-           'd) failure of militaristic states like Germany, Italy and Japan to effectively build and mobilize large standing armies.']
-qodAns2 = ['a) These leaders were eager to make full use of the wide variety of new, extremely deadly weapons introduced in World War I.','\n',
-           'b) These leaders agreed to avoid the use of the wide variety of new, extremely deadly weapons introduced in World War I should fighting start again','\n',
-           'c) These leaders were reluctant to enter another major war, so they gave in to many of Hitler\'s demands.','\n',
-           'd) These leaders were sure that, should another major war start, this time Germany would be quickly and easily defeated.']
+qodAnswers = ['a) Nearly all European nations adopted communist forms of government after World War I.','\n','b) Communists also seized power in Great Britain and France after World War I.','\n','c) The formation of the League of Nations can be seen as a similar consolidation of power under one totalitarian governing body.','\n','d) Germany and Italy also embraced totalitarian dictatorships in response to economic, political and social problems arising out of World War I.']
+qodAns1 = ['a) decline of totalitarian leaders whose militaristic policies led to more costly wars.','\n','b) rise of totalitarian leaders worldwide who promised their citizens a return to stability through strong leadership and militaristic expansion.','\n','c) rise of totalitarian leaders worldwide whose message of isolationism and appeasement indicated a lack of concern for foreign affairs.','\n','d) failure of militaristic states like Germany, Italy and Japan to effectively build and mobilize large standing armies.']
+qodAns2 = ['a) These leaders were eager to make full use of the wide variety of new, extremely deadly weapons introduced in World War I.','\n','b) These leaders agreed to avoid the use of the wide variety of new, extremely deadly weapons introduced in World War I should fighting start again','\n','c) These leaders were reluctant to enter another major war, so they gave in to many of Hitler\'s demands.','\n','d) These leaders were sure that, should another major war start, this time Germany would be quickly and easily defeated.']
 def ask_questions():
     points = 0
     print(qodQuestions[0])
