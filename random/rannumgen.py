42fb3bc41baf72e9e462e061d26df9c0a0f9dad2
devonk15 <94760159+devonk15@users.noreply.github.com>
2023-04-18 15:11:13 -0400

    Update rannumgen.py

--- random/rannumgen.py
+++ random/rannumgen.py
@@ -1,1 +1,9 @@
+import time
 
+def countdown():
+  global timepas
+  timepas = float(input('Hello! How much time do you want to pass?'))
+  while timepas != 0:
+    timepas -= 0.01
+    print(timepas)
+    time.sleep(0.5)
