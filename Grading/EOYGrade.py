a3fdb679198bfb3bd7c144886a146da6a5eaea7a
devonk15 <94760159+devonk15@users.noreply.github.com>
2023-05-19 11:32:02 -0400

    Update EOYGrade.py

--- Grading/EOYGrade.py
+++ Grading/EOYGrade.py
@@ -1,1 +1,11 @@
+import time
 
+q1 = int(input('Quarter 1 Grade: ')); q2 = int(input('Quarter 2 Grade: ')); q3 = int(input('Quarter 3 Grade: ')); q4 = int(input('Quarter 4 Grade: '))
+time.sleep(0.5)
+print('Calculating...\n'); time.sleep(3)
+
+quarters = q1+q2+q3+q4
+
+eoygrade = quarters/4
+
+print('Done!\n'); time.sleep(1); print(f'EOYGrade: {eoygrade}%')
