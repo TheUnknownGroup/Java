2be123ac0a9db246d527e7121e44bb908087496f
devonk15 <kaylordevon5@gmail.com>
2023-10-01 10:40:17 -0400

    Update new.py Create ass.py

---
+++ ass.py
@@ -0,0 +1,7 @@
+
+
+choice = input("Do you want ass? (yes/no): ")
+if choice in ("yes", "Yes", "y", "sure"):
+    print("Sorry, there is no ass in stock.")
+else:
+    print("Well, you're in luck! We have cake instead.")
--- new.py
+++ new.py
@@ -11,13 +11,14 @@ def new2():
     inp = input(f'{line}\nYour choice: ')
     thanks = f'Thanks for choosing {inp}'
     if inp is ['a', 'b', 'c', 'd', 'e']:
-        print(thanks); exit()
-    elif inp not in ['a', 'b', 'c', 'd', 'e']:
+        print(thanks); time.sleep(0.5) 
+        exit()
+    else:
         print('Please choose: a, b, c, d, e'); time.sleep(1); new()
 
 
 def new():
-    print(start + '\n'+ line); time.sleep(0.35)
+    print(start +'\n'+ line); time.sleep(0.35)
     for s in list:
         print(s, end='' + '\n')
     new2()
