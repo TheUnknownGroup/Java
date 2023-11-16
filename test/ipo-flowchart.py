b0084f0a42ccf1f2a8c6916b73381802ccdb3a46
devonk15 <94760159+devonk15@users.noreply.github.com>
2023-11-16 09:18:28 -0500

    done

--- test/ipo-flowchart.py
+++ test/ipo-flowchart.py
@@ -1,1 +1,19 @@
+import time
+line = '------'
 
+questions = ['What is your Username?\n',
+             'What is your Password?\n']
+username = 'ddr4lover'
+password = 'ddr5lover#5'
+
+welcome = 'Welcome! Please Log In with the correct credentials.'
+
+def login():
+    print(welcome); time.sleep(0.5)
+    for s in questions[0]:
+        print(s, end='')
+    ans1 = input('')
+    for s in questions[1]:
+        print(s, end='')
+
+login()
