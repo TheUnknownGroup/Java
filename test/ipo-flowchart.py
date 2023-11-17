f4164df834a1cbb2c6e80dd04f5bf5c369636c65
devonk15 <94760159+devonk15@users.noreply.github.com>
2023-11-17 10:46:37 -0500

    Update

--- test/ipo-flowchart.py
+++ test/ipo-flowchart.py
@@ -7,6 +7,8 @@ username = 'ddr4lover'
 password = 'ddr5lover#5'
 
 welcome = 'Welcome! Please Log In with the correct credentials.'
+cor = 'incorrect'
+inc = 'correct'
 
 def login():
     print(welcome); time.sleep(0.5)
@@ -15,5 +17,18 @@ def login():
     ans1 = input('')
     for s in questions[1]:
         print(s, end='')
-
+    ans2 = input('')
+    time.sleep(1)
+    if ans1 == username:
+        ans1 = 'correct'
+    elif ans1 != username:
+        ans1 = 'incorrect'
+    elif ans2 == passwod:
+        ans1 = 'correct'
+    elif ans2 != password:
+        ans2 = 'incorrect'
+    elif ans1 and ans2 == 'incorrect':
+        print('Incorrect password & username. Please Try Again.')
+    elif ans1 and ans2 == 'correct':
+        print('Logging In')
 login()
